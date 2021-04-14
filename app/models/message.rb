d6ea59a4c440efc50b663933f29b5d7ae9bf47eb
airatwn <o98.ogawa.yuta@gmail.com>
2021-04-14 11:28:20 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
 
+  def was_attached?
+    self.image.attached? #モデル.ファイル名.attched?
+  end
 end
